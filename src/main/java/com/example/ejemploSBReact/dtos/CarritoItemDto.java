80a8741c1f8b66d0b7466a85f6bdabb7ee4adadf
Dany01627 <dani.oliveros@duocuc.cl>
2025-12-10 23:11:38 -0300

    avance carrito

---
+++ src/main/java/com/example/ejemploSBReact/dtos/CarritoItemDto.java
@@ -0,0 +1,12 @@
+package com.example.ejemploSBReact.dtos;
+
+// CarritoItemDto.java
+public class CarritoItemDto {
+    private Long productoId;
+    private String nombre;
+    private Integer precioUnitario;
+    private Integer cantidad;
+    private String imagenUrl;
+
+    // getters y setters
+}
