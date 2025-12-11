6654aefbffe9a8475802edd7d6ec229b1751c3f0
Constanza Raquel Pino Villa <co.pinov@duocuc.cl>
2025-12-10 22:43:59 -0300

    hago todo mi backend de springboot (productos y categorías)

--- src/main/java/com/example/ejemploSBReact/repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ejemploSBReact.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ejemploSBReact.model.Persona;
-
-public interface PersonaRepository extends JpaRepository<Persona,Long> {
-  // hereda los medotos de JpaRepository    
-} 
---
+++ src/main/java/com/example/ejemploSBReact/repository/ProductoRepository.java
@@ -0,0 +1,8 @@
+package com.example.ejemploSBReact.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ejemploSBReact.model.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
